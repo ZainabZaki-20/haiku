4cfc80cd6e0fcc2aafe4f18d56fdd747ae954e87
ZainabZaki-20 <Zainab.Zaki.2025@gmail.com>
2026-04-08 23:14:49 -0500

    Enhance main.js with randomization features
    Refactor randomNumber function and add randomize functionality.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,28 @@
 // main.js
-// main.js
-let rotation = 0;
-document.addEventListener ("click", function () {
-    let min = 10;
-    let max = 40;
-    rotation += randomNumber (1,200);
-    document.body.style.transform = 'rotate(${rotation}deg)';
+document.querySelectorAll(".wind").forEach(div => {
+    div.style.opacity = 0.5;
 });
-function randomNumber (min, max){
-    return Math.floor(Math.random() * (max - min) +1) + min;
+    
+
+function randomNumber (min,max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+let divs = document.querySelectorAll (".grid-container div")
+function randomize(){
+divs.forEach(function(div){
+let scale = randomNumber(.5, 1.2);
+let translateX = randomNumber(0, 50);
+let translateY = randomNumber(0, 20);
+let rotate = randomNumber(0, 360);
+div.style.transform =
+`scale(${scale})
+translate(${translateX}%, ${translateY}%)
+rotate(${rotate}deg)`;
+})
+console.log(divs.length, "randomize! ");
+}
+document.addEventListener("click", randomize);
+
+
+
 
